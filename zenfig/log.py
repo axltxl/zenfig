b3b76efa2924aa4fcd000502f955f93c930aedfa
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2016-01-29 11:03:17 -0430

    Basic code infrastructure

---
+++ zenfig/log.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+"""
+zenfig.log
+~~~~~~~~~~~~~
+
+Nice output
+
+:copyright: (c) 2015 by Alejandro Ricoveri
+:license: MIT, see LICENSE for more details.
+"""
+
+
+import sys
+from clint.textui.colored import white, red, cyan, yellow, green
+from clint.textui import puts
+
+# Globals
+_stdout = False
+
+
+def init(*, quiet_stdout=False):
+    """
+    Initiate the log module
+
+    :param threshold_lvl: messages under this level won't be issued/logged
+    :param to_stdout: activate stdout log stream
+    """
+
+    # create stout handler
+    if not quiet_stdout:
+        global _stdout
+        _stdout = True
+
+
+def to_stdout(msg, *, colorf=green, bold=False):
+    if _stdout:
+        print(colorf(msg, bold=bold), file=sys.stderr)
+
+
+
+def msg(message, *, bold=False):
+    """
+    Log a regular message
+
+    :param message: the message to be logged
+    """
+    to_stdout(" --- {message}".format(message=message), bold=bold)
+
+
+def msg_warn(message):
+    """
+    Log a warning message
+
+    :param message: the message to be logged
+    """
+    to_stdout(" (!) {message}".format(message=message),
+              colorf=yellow, bold=True)
+
+
+def msg_err(message):
+    """
+    Log an error message
+
+    :param message: the message to be logged
+    """
+    to_stdout(" !!! {message}".format(message=message), colorf=red, bold=True)
+
+
+def msg_debug(message):
+    """
+    Log a debug message
+
+    :param message: the message to be logged
+    """
+    to_stdout(" (*) {message}".format(message=message), colorf=cyan)
