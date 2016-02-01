ff2b648ec88cb57eda3d65ceaa0dfa070c4a89da
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2016-02-01 07:28:12 -0430

    get_xdg_cache_home initial implementation

--- zenfig/util.py
+++ zenfig/util.py
@@ -14,6 +14,16 @@ import os
 
 from . import __name__ as pkg_name
 
+def get_xdg_cache_home():
+    """Get XDG_CACHE_HOME"""
+
+    # XDG_CACHE_HOME/zenfig/templates is inside
+    # the template search path
+    xdg_cache_home = os.getenv('XDG_CACHE_HOME')
+    if xdg_cache_home is None:
+        xdg_cache_home = "{}/.cache/{}".format(os.getenv("HOME"), pkg_name)
+    return xdg_cache_home
+
 def get_xdg_data_home():
     """Get XDG_DATA_HOME"""
 
